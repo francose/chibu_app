ebb0fdfbe784e0b7ac8f2add0f4b5de4c7b06aaa
Sadik <elmanoukdesign@gmail.com>
2024-01-13 17:04:51 -0500

    converted ts

---
+++ README.md
@@ -0,0 +1,6 @@
+# Chibu DevSecOps Management Platform:
+
+OS Patching
+code scanning
+Site Penetration Testing
+
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const rootElement = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
